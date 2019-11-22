989b7b1c3b00154774721d3a515a03596ce55bef
buenasbuenassergio <sergiomartinmmir@gmail.com>
2019-11-22 13:13:41 +0100

    Ampliando ejercicios

---
+++ Ejercicio Bucles/Ejercicio 01.py
@@ -0,0 +1,4 @@
+word = "Gato Lopez"
+
+for i in range(10):
+    print(word)
---
+++ Ejercicio Bucles/Ejercicio 03.py
@@ -0,0 +1,4 @@
+n = int(input("Introduce un numero cualquiera: "))
+
+for i in range(1, n + 1 , 2):
+        print(i , end= ",")
